16f2615bcbb312b3b607e055b6c55902d3255d68
Svitolda <sveta20000203@gmail.com>
2023-04-07 16:56:57 +0200

    Update-02

--- src/js/02-video.js
+++ src/js/02-video.js
@@ -1,1 +1,14 @@
+import Player from "@vimeo/player";
+import _throttle from "lodash.throttle";
+
+const currentTimeStorage = "videoplayer-current-time";
+
+const iframe = document.querySelector("iframe");
+const player = new Vimeo.Player(iframe);
+
+player.on('timeUpdate', throttle(onPlay, 1000));
+
+
+
+
 
