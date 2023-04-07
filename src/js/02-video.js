045f1d57347fb159dcdd56deeadd58bf1b937734
Svitolda <sveta20000203@gmail.com>
2023-04-08 00:45:12 +0200

    update03

--- src/js/02-video.js
+++ src/js/02-video.js
@@ -1,5 +1,5 @@
-import Player from "@vimeo/player";
-import _throttle from "lodash.throttle";
+import player from "@vimeo/player";
+import throttle from "lodash.throttle";
 
 const currentTimeStorage = "videoplayer-current-time";
 
@@ -8,6 +8,18 @@ const player = new Vimeo.Player(iframe);
 
 player.on('timeUpdate', throttle(onPlay, 1000));
 
+function onPlay (event) {
+    // data is an object containing properties specific to that event
+    localStorage.setItem(currentTimeStorage, event.seconds)
+};
+    setCurrentTime();
+    function setCurrentTime() {
+        if (!localStorage.getItem(currentTimeStorage)) {
+            return;
+        }
+        player.setCurrentTime(localStorage.getItem(currentTimeStorage));
+    }
+player.on('play', onPlay);
 
 
 
--- src/js/03-feedback.js
+++ src/js/03-feedback.js
@@ -1,1 +1,21 @@
+// import throttle from "lodash.throttle"; 
 
+// const STORAGE_KEY = feedback-msg;
+
+const form = document.querySelector(".feedback-form");
+
+const textarea = document.querySelector(".feedback-form textarea");
+
+form.addEventListener('submit', onFormSubmit);
+textarea.addEventListener('submit', onTextInput);
+
+function onFormSubmit(evt) {
+    // evt.preventDefault();
+    // evt.currentTarget.reset();
+}
+
+function onTextInput(evt) {
+    const message = evt.currentTarget.value;
+    
+    localStorage.setItem('feedback-msg', message);
+}
