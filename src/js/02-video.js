837db0e007df4380d5fc3ec9228ee0a142c873b5
Svitolda <sveta20000203@gmail.com>
2023-04-08 16:48:57 +0200

    Update 02-video.js

--- src/js/02-video.js
+++ src/js/02-video.js
@@ -6,7 +6,7 @@ const CURRENT_TIME = 'videoplayer-current-time';
 const iframe = document.querySelector('iframe');
 const player = new Player(iframe);
 
-player.on('uppdate-time', throttle(onPlay, 1000));
+player.on(' timeupdate', throttle(onPlay, 1000));
 function onPlay(event) {
   localStorage.setItem(CURRENT_TIME, event.seconds);
 }
