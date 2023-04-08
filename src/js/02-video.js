bcbe56863a35241a4b3ec22c486fd4fce6ceb53f
Svitolda <sveta20000203@gmail.com>
2023-04-08 14:42:40 +0200

    Updatw2faile

--- src/js/02-video.js
+++ src/js/02-video.js
@@ -1,26 +1,41 @@
-import player from "@vimeo/player";
-import throttle from "lodash.throttle";
+import Player from '@vimeo/player';
+import throttle from 'lodash.throttle';
 
 const currentTimeStorage = "videoplayer-current-time";
-
 const iframe = document.querySelector("iframe");
-const player = new Vimeo.Player(iframe);
+const player = new Player(iframe);
 
-player.on('timeUpdate', throttle(onPlay, 1000));
+player.on('timeUpdate', throttle(function () {
+    player.getCurrentTime().then(function (seconds) {
+        localStorage.setItem(currentTimeStorage, seconds);
+    })
+}, 1000));
 
-function onPlay (event) {
-    // data is an object containing properties specific to that event
-    localStorage.setItem(currentTimeStorage, event.seconds)
-};
-    setCurrentTime();
-    function setCurrentTime() {
-        if (!localStorage.getItem(currentTimeStorage)) {
-            return;
-        }
-        player.setCurrentTime(localStorage.getItem(currentTimeStorage));
-    }
-player.on('play', onPlay);
+const storedTime = localStorage.getItem(currentTimeStorage);
+if (storedTime) {
+    player.setCurrentTime(storedTime);
+}
 
+// const onPlay = function (evt) {
+//     localStorage.setItem(currentTimeStorage, evt.seconds);
+// };
+// player.on('timeUpdate', throttle(onPlay, 1000));
+
+// const currentTime = Number(localStorage.getItem(currentTimeStorage));
+
+// player.setCurrentTime(currentTime).then(function(seconds) {
+//     // seconds = the actual time that the player seeked to
+// }).catch(function(error) {
+//     switch (error.name) {
+//         case 'RangeError':
+//             // the time was less than 0 or greater than the video’s duration
+//             break;
+
+//         default:
+//             // some other error occurred
+//             break;
+//     }
+// });
 
 
 
