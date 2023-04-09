da7ce7c3b20dfc20d2a72bd9e6cf58a2da7e63d0
Svitolda <sveta20000203@gmail.com>
2023-04-09 22:34:52 +0200

    03final

--- src/js/03-feedback.js
+++ src/js/03-feedback.js
@@ -1,38 +1,34 @@
-import throttle from "lodash.throttle"; 
+import throttle from 'lodash.throttle';
 
 const STORAGE_KEY = 'feedback-msg';
 let formData = JSON.parse(localStorage.getItem(STORAGE_KEY)) || {};
-const form = document.querySelector("[required]");
-// const textarea = document.querySelector(".feedback-form textarea");
+const form = document.querySelector('.feedback-form');
 onPageReload();
-
 
 form.addEventListener('input', throttle(onInputChange, 500));
 form.addEventListener('submit', onBtnSubmit);
 
 function onInputChange(evt) {
-    formData[evt.target.name] = evt.target.value;
-    localStorage.setItem(STORAGE_KEY,  JSON.stringify(formData));
-    // evt.currentTarget.reset();
-    
+  formData[evt.target.name] = evt.target.value;
+  localStorage.setItem(STORAGE_KEY, JSON.stringify(formData));
 }
-
 function onBtnSubmit(evt) {
-    evt.preventDefault();
-    if (formData.email === '' || formData.message === ''){
-        return alert ('Заповніть форму');
-    }
-    evt.target.reset();
-    console.log(formData);
-    localStorage.removeItem(STORAGE_KEY);
-    formData = {};
+  evt.preventDefault();
+  const { email, message } = evt.currentTarget;
+  if (email.value === '' || message.value === '') {
+    return alert('Заповніть форму');
+  }
+  evt.target.reset();
+  console.log(formData);
+  localStorage.removeItem(STORAGE_KEY);
+  formData = {};
 }
-
-
 function onPageReload() {
-     if (formData) {
-      let { email, message } = form.elements;
-      email.value = formData.email || '';
-      message.value = formData.message || '';
-    }
+  if (formData) {
+    const entries = Object.entries(formData);
+    entries.forEach(function (el) {
+      const [key, value] = el;
+      form[key].value = value;
+    });
+  }
 }
